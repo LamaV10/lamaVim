b1f88c730d70f00fbf2e2ff914e37bcdbc70aa3c
LamaV10 <lama.customs@gmail.com>
2025-05-17 23:00:55 +0200

    ctrl h j k l keybinds added for changing windows.

--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ vim.g.mapleader = ","
 --- indenting width
 vim.opt.shiftwidth = 4
 
+--- change windows with h j k l
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
 --- packagemanager lazy 
 require("config.lazy")
 
