62c95fe7ff72a8098d1fcf51e109b027bde5f7c3
LamaV10 <lama.customs@gmail.com>
2025-05-22 15:09:59 +0200

    Fixed install.sh.

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-if [ ! -d "$~/.config/nvim" ]; then
-    mkdir ~/.config/nvim
-    echo "Config directory has been made!"
+if [ -d "$~/.config/nvim" ]; then
+    mkdir ~/Nvim-Backup
+    echo "Move old conifg into ~/.config/nvim/Backup"
+    mv ~/.config/nvim ~/Nvim-Backup
 fi
 
-cd ~/.config/nvim
-mkdir Backup
-echo "Move old conifg into ~/.config/nvim/Backup"
-mv ~/.config/nvim ~/.config/nvim/Backup
 echo "Install config!"
 git clone https://github.com/LamaV10/lamaVim ~/.config/nvim
 echo "Done! Open nvim to see the changes!"
