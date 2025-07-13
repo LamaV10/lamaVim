d6ac26929d180a042201b1d6d2b94a8d9f206ea6
LamaV10 <lama.customs@gmail.com>
2025-07-13 20:22:50 +0200

    Added rust-analyzer to the lspconfig.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,10 +2,11 @@ return {
     {
 	"neovim/nvim-lspconfig",
 	config = function ()
-	   vim.lsp.enable('ccls') 
-	   vim.lsp.enable('jdtls')
-	   vim.lsp.enable('pylsp')
-	   vim.lsp.config('pylsp', {
+	    vim.lsp.enable('ccls') 
+	    vim.lsp.enable('jdtls')
+	    vim.lsp.enable('pylsp')
+	    vim.lsp.enable('rust_analyzer')
+	    vim.lsp.config('pylsp', {
 	       cmd = { "/home/marcel/.venv/nvim/bin/pylsp" },
 	       settings = {
 		   pylsp = {
