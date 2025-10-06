a91b7cf89b88308466fae1e468c39b23bcab771e
LamaV10 <lama.customs@gmail.com>
2025-10-06 10:18:40 +0200

    Turned virtual edit back on && modified pylsp to show even fewer warnings.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,23 @@
 return {
-    {
-	"neovim/nvim-lspconfig",
-	config = function ()
-	    vim.lsp.enable('ccls') 
-	    vim.lsp.enable('jdtls')
-	    vim.lsp.enable('rust_analyzer')
-	    vim.lsp.enable('htmx')
-	    vim.lsp.enable('pylsp')
-	    vim.lsp.config('pylsp', {
-	       cmd = { "/home/marcel/.venv/nvim/bin/pylsp" },
-	       settings = {
-		   pylsp = {
-		       plugins = {
-			   pycodestyle = {
-			       ignore = {'W391', 'E305', 'E302', 'E266', 'E303'},
-			       maxLineLength = 100
-			   }
-		       }
-		   }
-	       }
-	   })
-	end,
-    }
+    "neovim/nvim-lspconfig",
+    config = function ()
+	vim.lsp.enable('ccls') 
+	vim.lsp.enable('jdtls')
+	vim.lsp.enable('rust_analyzer')
+	vim.lsp.enable('htmx')
+	vim.lsp.enable('pylsp')
+	vim.lsp.config('pylsp', {
+	cmd = { "/home/marcel/.venv/nvim/bin/pylsp" },
+	    settings = {
+		pylsp = {
+		    plugins = {
+			pycodestyle = { enabled = false }, -- deactivates E/W style warnings
+			pydocstyle  = { enabled = false }, -- deactivates Dxxx docstring warnings
+			mccabe      = { enabled = false }, -- deactivates complexity warnings
+			pyflakes    = { enabled = true },  -- keeps error checks
+		    }
+		}
+	    }
+	})
+    end,
 }
